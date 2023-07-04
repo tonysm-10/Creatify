5d6c738e232c8f4fc001a7996f7147967b869fe8
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-07-03 21:11:30 -0700

    changed middleware

--- server/server.js
+++ server/server.js
@@ -17,27 +17,25 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-const startApolloServer = async (typeDefs, resolvers) => {
+async function startApolloServer() {
   await server.start();
+
   server.applyMiddleware({ app });
-  
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+  }
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-};
+    });
+  });
+}
 
-startApolloServer(typeDefs, resolvers);
+startApolloServer();
