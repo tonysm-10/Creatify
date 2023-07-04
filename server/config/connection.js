931a45cfde92607a46bcdeb3bcb5240b844ee72f
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-07-03 17:36:12 -0700

    fixed connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/creatifydb', {
+mongoose.connect('mongodb://localhost:27017/creatifydb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
